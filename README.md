6713eaf98bd025bbd0c9df48ed23e9f9fb15e6d7
Dilan Asiri <dilanasiri95@gmail.com>
2023-09-12 07:49:15 +0530

    find the sum of two numbers given array

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 <h1>Algorithm</h1>
 
 1.Fibonacci Sequence
+2.Min and Max Value
+3.Sum of two numbers given array
 
 #Copyright &copy 
---
+++ algorithm-1/algorithm-1/src/main/java/lk/dilan/algorithm/TwoSum.java
@@ -0,0 +1,32 @@
+package lk.dilan.algorithm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numIndices = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numIndices.containsKey(complement)) {
+                return new int[]{numIndices.get(complement), i};
+            }
+            numIndices.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        if (result != null) {
+            System.out.println("Indices of the two numbers: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
+
